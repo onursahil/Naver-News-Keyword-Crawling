7060a1a9ebaeb666cb2ddfab5126113906f75625
오누르 사힐제리트 <osc@xinapse.ai>
2019-08-06 18:55:44 +0900

    removed trivial parts of the code

--- src/keyword_timerange.py
+++ src/keyword_timerange.py
@@ -31,41 +31,47 @@ def crawlNewsContent(pre_url):
         for doc in news_blocks:
             link_list.append(doc.get_attribute("href"))
 
-        # THROW FOR LOOP FOR EACH NEWS LINK
-        for element in link_list:
-            news_content = {}
-            driver.get(element)
-            driver.implicitly_wait(1)
+        try:
+            # THROW FOR LOOP FOR EACH NEWS LINK
+            for element in link_list:
+                news_content = {}
+                driver.get(element)
+                driver.implicitly_wait(1)
 
-            # EXTRACT PUBLISH DATE
-            publish_date = driver.find_element_by_xpath("//span[@class='t11']").text
-            if '. 오후' in publish_date:
-                publish_date = publish_date.replace(". 오후", "")
-            else:
-                publish_date = publish_date.replace(". 오전", "")
+                # EXTRACT PUBLISH DATE
+                publish_date = driver.find_element_by_xpath("//span[@class='t11']").text
+                if '. 오후' in publish_date:
+                    publish_date = publish_date.replace(". 오후", "")
+                else:
+                    publish_date = publish_date.replace(". 오전", "")
 
-            news_content["published_date"] = datetime.datetime.strptime(publish_date, "%Y.%m.%d %I:%M")
+                news_content["published_date"] = datetime.datetime.strptime(publish_date, "%Y.%m.%d %I:%M")
 
-            # EXTRACT NEWS CONTENT
-            content_xpath = "//div[@id='articleBodyContents']"
-            news_content["news"] = driver.find_element_by_xpath(content_xpath).text
+                # EXTRACT NEWS CONTENT
+                content_xpath = "//div[@id='articleBodyContents']"
+                news_content["news"] = driver.find_element_by_xpath(content_xpath).text
 
-            print(json.dumps(news_content, ensure_ascii=False, default=default))
+                print(json.dumps(news_content, ensure_ascii=False, default=default))
 
-            time.sleep(random.randint(1, 3))
-            driver.back()
-            time.sleep(random.randint(1, 3))
+                time.sleep(random.randint(1, 3))
+                driver.back()
+                time.sleep(random.randint(1, 3))
+        except NoSuchElementException as e:
+            last_date = publish_date
+            print(last_date)
+
 
 def default(o):
     if isinstance(o, (datetime.date, datetime.datetime)):
         return o.isoformat()
+
 
 # ADJUST THE TIME RANGE FOR 10 YEARS
 def adjustDate():
     today_date = datetime.datetime.now()
     today = today_date.strftime("%Y-%m-%d")
     today = today.replace("-", ".")
-    ten_years = today_date - relativedelta(years=10)
+    ten_years = today_date - relativedelta(months=3)
     ten_years_back = ten_years.strftime("%Y-%m-%d")
     ten_years_back = ten_years_back.replace("-", ".")
 
@@ -73,6 +79,7 @@ def adjustDate():
     ten_year_back_straight = ten_years_back.replace(".", "")
 
     return today, ten_years_back, today_straight, ten_year_back_straight
+
 
 # GET THE KEYWORD TO SEARCH FOR
 def createURL():
@@ -91,6 +98,7 @@ def createURL():
     crawlNewsContent(pre_url)
     print(datetime.datetime.now().time())
 
+
 # INITIALIZE CHROMEDRIVER
 def initDriver():
     options = webdriver.ChromeOptions()
@@ -101,6 +109,7 @@ def initDriver():
     if not driver:
         driver = webdriver.Chrome("/Users/onursahil/Documents/Developer/Naver-News-Keyword-Crawling/chromedriver", chrome_options=options)
     createURL()
+
 
 if __name__ == '__main__':
     initDriver()
